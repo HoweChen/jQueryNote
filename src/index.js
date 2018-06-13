0b6e96aa6112912bcf57db04b6977864ee0c3f1d
SkyFather <cyh278889789@gmail.com>
2018-06-14 00:16:55 +0800

    add build files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import * as http from "http";
+import * as url from "url";
+import * as $ from "jquery";
+
+http
+  .createServer(function(req, res) {
+    console.log("Request: " + req.method + " to " + req.url);
+    res.writeHead(200, "OK");
+    res.write("<h1>Hello</h1> Node.js is working");
+    res.end();
+  })
+  .listen(8080);
+
+console.log("Ready on port 8080");
